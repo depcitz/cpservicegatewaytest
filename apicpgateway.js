6bd5583d5a09d8d81b3f5b613639a9dc40516ff2
CIT_BANNAYUU <citudemy2@gmail.com>
2021-10-19 12:15:35 +0700

    Initial Project

--- apicpgateway.js
+++ apicpgateway.js
@@ -7,19 +7,6 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    var responseObject = {
-        status: true,
-        data: { text: "hello" }
-    } 
-    res.json(responseObject);
-});
-
-app.listen(port, () => {
-   console.log("application is listening on:", port);
-});
-
-
 
 app.get("/", (req, res) => {
     var responseObject = {
